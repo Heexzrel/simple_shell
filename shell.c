31add499c305feb83791e5d814e45753d02c7d78
George Israel <georgeisrael22@gmail.com>
2023-05-21 14:37:24 -0700

    simple_shell

---
+++ shell.c
@@ -0,0 +1,122 @@
+#include "shell.h"
+
+/**
+main - Entry point of the program. Initializes program variables.
+@argc: Number of values received from the command line.
+@argv: Values received from the command line.
+@env: Environment variables received from the command line.
+Return: Returns zero on success.
+*/
+int main(int argc, char *argv[], char *env[])
+{
+	data_of_program data_struct = {NULL}, *data = &data_struct;
+	char *prompt = "";
+
+	inicialize_data(data, argc, argv, env);
+
+	signal(SIGINT, handle_ctrl_c);
+
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
+	{
+		errno = 2;
+		prompt = PROMPT_MSG;
+	}
+	errno = 0;
+	sisinfo(prompt, data);
+	return (0)
+}
+
+/**
+handle_ctrl_c - Signal handler for SIGINT (ctrl + c) signal.
+@opr: Option of the prototype (UNUSED).
+*/
+void handle_ctrl_c(int opr UNUSED)
+{
+	_print("\n");
+	_print(PROMPT_MSG);
+}
+
+/**
+initialize_data - Initializes the data structure with program information.
+@data: Pointer to the structure of data.
+@argv: Array of arguments passed to the program execution.
+@env: Environment variables passed to the program execution.
+@argc: Number of values received from the command line.
+*/
+void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
+{
+	int i = 0;
+
+	data->program_name = argv[0];
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->exec_counter = 0;
+	
+	if (argc == 1)
+		data->file_descriptor = STDIN_FILENO;
+	else
+	{
+		data->file_descriptor = open(argv[1], O_RDONLY);
+		if (data->file_descriptor == -1)
+		{
+			_printe(data->program_name);
+			_printe(": 0: Can't open ");
+			_printe(argv[1]);
+			_printe("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (; env[i]; i++)
+		{
+			data->env[i] = str_duplicate(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+
+	data->alias_list = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->alias_list[i] = NULL;
+	}
+
+}
+
+/**
+sisifo - Infinite loop that displays the prompt and executes commands.
+@prompt: Prompt to be printed.
+@data: Pointer to the program's data structure.
+*/
+void sisifo(char *prompt, data_of_program *data)
+{
+	int error_code = 0, string_len = 0;
+
+	while (++(data->exec_counter))
+	{
+		_print(prompt);
+		error_code = string_len = _getline(data);
+
+		 if (error_code == EOF)
+		{
+			free_all_data(data);
+			exit(errno); 
+		}
+		if (string_len >= 1)
+		{
+			expand_alias(data);
+			expand_variables(data);
+			tokenize(data);
+			if (data->tokens[0])
+			{ /* execute if a text is given to the  prompt. */
+				error_code = execute(data);
+				if (error_code != 0)
+					_print_error(error_code, data);
+			}
+			free_recurrent_data(data);
+		}
+	}
+}
