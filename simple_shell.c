f28183486da51b8ff9c7a540d16d9f9104655ff7
George Israel <georgeisrael22@gmail.com>
2023-05-24 16:16:58 -0700

    simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -7,56 +7,75 @@
  * Return: Exit status
  */
 
+char **parse_cmd(char *cmd);
+
 int main(__attribute__((unused)) int argc, char **argv)
 {
-	char *input, **cmd;
-	int counter = 0, status = 1, st = 0;
+    char *input, **cmd;
+    int counter = 0, statue = 1, st = 0;
 
-	/* Read commands from a file if specified*/
-	if (argv[1] != NULL)
-		read_file(argv[1], argv);
-	signal(SIGINT, signal_to_handel);
-
-	while (status)
-	{
-		counter++;
-
-		/* Display the shell prompt if running interactively*/
-		if (isatty(STDIN_FILENO))
-			prompt();
-
-		/* Read input from the user*/
-		input = _getline();
-
-		if (input[0] == '\0')
-		{
-			continue;
-		}
-
-		history(input);
-
-		/* Check if the command is "exit"*/
-		if (_strcmp(cmd[0], "exit") == 0)
-		{
-			exit_bul(cmd, input, argv, counter);
-		}
-		/* Check if the command is a built-in command*/
-		else if (check_builtin(cmd) == 0)
-		{
-			st = handle_builtin(cmd, st);
-			free_all(cmd, input);
-			continue;
-		}
-		else
-		{
-			st = check_cmd(cmd, input, counter, argv);
-		}
-
-		free_all(cmd, input);
-	}
-
-	return (status);
+    if (argv[1] != NULL)
+        read_file(argv[1], argv);
+    signal(SIGINT, signal_to_handel);
+    while (statue)
+    {
+        counter++;
+        if (isatty(STDIN_FILENO))
+            prompt();
+        input = _getline();
+        if (input[0] == '\0')
+        {
+            continue;
+        }
+        history(input);
+        cmd = parse_cmd(input);
+        if (cmd == NULL) {
+            free(input);
+            continue;
+        }
+        if (_strcmp(cmd[0], "exit") == 0)
+        {
+            exit_bul(cmd, input, argv, counter);
+        }
+        else if (check_builtin(cmd) == 0)
+        {
+            st = handle_builtin(cmd, st);
+            free_all(cmd, input);
+            continue;
+        }
+        else
+        {
+            st = check_cmd(cmd, input, counter, argv);
+        }
+        free_all(cmd, input);
+    }
+    return (statue);
 }
+
+/**
+ * parse_cmd - Parse the command line into individual arguments
+ */
+char **parse_cmd(char *cmd)
+{
+    char **args = malloc(sizeof(char *) * MAX_ARGS);
+    if (args == NULL) {
+        return (NULL);
+    }
+
+    char *token;
+    int arg_count = 0;
+
+    token = strtok(cmd, " ");
+    while (token != NULL && arg_count < MAX_ARGS - 1) {
+        args[arg_count] = token;
+        arg_count++;
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL;
+
+    return (args);
+}
+
 
 /**
  * check_builtin - Check if the command is a built-in command
